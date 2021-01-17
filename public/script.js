82905db508821a8aba1f0859ca059aee52ead785
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2021-01-16 22:53:04 -0500

    Create event listener for error button
    Error messages can now be hidden with close button

--- public/script.js
+++ public/script.js
@@ -1,1 +1,10 @@
-const closeErrorButton = document.getElementById('hideErrorButton');
+const closeErrorButtons = document.querySelectorAll('.error-button');
+
+// close error messages
+closeErrorButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    e.target.parentElement.classList.add('hide');
+  });
+});
